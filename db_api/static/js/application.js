162d1a51dc44ae2feb73a2b3cae47800f2370b4a
chengjf <sharpkingdom@gmail.com>
2015-08-10 10:46:51 +0800

    将编辑的保存和新建的保存分开处理，将编辑的取消和新建的取消分开处理。

--- db_api/static/js/application.js
+++ db_api/static/js/application.js
@@ -1,120 +1,122 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
-  this.resource("systems");
+	// put your routes here
+	this.resource("systems");
 });
 
 App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    // return ['red', 'yellow', 'blue'];
-    return this.store.findAll('system');
-  }
+	model: function() {
+		// return ['red', 'yellow', 'blue'];
+		return this.store.findAll('system');
+	}
 });
 
 App.IndexController = Ember.Controller.extend({
-  actions: {
-    doSomething: function(){
-      alert('IndexController');
-    }
-  }
+	actions: {
+		doSomething: function() {
+			alert('IndexController');
+		}
+	}
 });
 
 
 
 App.SystemsRoute = Ember.Route.extend({
-  model: function() {
-    return this.store.findAll('system');
-  },
-  setupController: function(controller,model) {
-     controller.set('model',model);
-  }
+	model: function() {
+		return this.store.findAll('system');
+	},
+	setupController: function(controller, model) {
+		controller.set('model', model);
+	}
 });
 
 App.SystemsController = Ember.Controller.extend({
-  actions: {
-    doSomething: function(){
-      alert('SystemsController');
-    },
+	actions: {
+		doSomething: function() {
+			alert('SystemsController');
+		},
 
-    add: function(){
-      var system = this.store.createRecord('system');
-      Em.Logger.info(system.get('controller'));
-      system.set('isCreating', 'true');
-    }
-  }
+		add: function() {
+			var system = this.store.createRecord('system');
+			Em.Logger.info(system.get('controller'));
+			system.set('isCreating', 'true');
+		}
+	}
 });
 
 
 App.SystemController = Ember.ObjectController.extend({
-  isEditing: false,
-  
-  isCreating: Ember.computed('id', 'name', 'desc',function() {
-    var flag = this.get('model').get('isCreating')
-    return flag === undefined ? false : flag;
-  }),
+	isEditing: false,
 
-  actions: {
-    edit: function() {
-      this.set('isEditing', true);
-    },
-    save: function() {
-      Em.Logger.info("SystemController save");
-      this.set('isEditing', false);
-      this.set('isCreating', false);
-      this.get("model").save();
-      Em.Logger.info(this.get("model").get('isCreating'));
-    },
-    delete: function(){
-      if (window.confirm('Are you sure you want to delete this record?')) {
-        var system = this.get("model");
-        system.deleteRecord();
-        system.save();
-      }
-    },
-    cancelEdit: function( model ) {
-      var system = this.get("model");
-      this.set('isEditing', false);
-    },
-    cancelSave: function( model ) {
-      var system = this.get("model");
-      system.deleteRecord();
-    },
-    create: function(){
-      var system = this.get("model");
+	isCreating: Ember.computed('id', 'name', 'desc', function() {
+		var flag = this.get('model').get('isCreating')
+		return flag === undefined ? false : flag;
+	}),
 
-      Em.Logger.info(system.get('id'));
-      system.save();
-    }
-  }
+	actions: {
+		edit: function() {
+			this.set('isEditing', true);
+		},
+		save: function() {
+			Em.Logger.info("SystemController save");
+			this.set('isEditing', false);
+			this.get('model').set('isCreating', false);
+			this.get("model").save();
+			Em.Logger.info(this.get("model").get('isCreating'));
+		},
+		delete: function() {
+			if (window.confirm('Are you sure you want to delete this record?')) {
+				var system = this.get("model");
+				system.deleteRecord();
+				system.save();
+			}
+		},
+		cancelEdit: function(model) {
+			var system = this.get("model");
+			system.rollback();
+			this.set('isEditing', false);
+		},
+		cancelSave: function(model) {
+			var system = this.get("model");
+			system.deleteRecord();
+		},
+		create: function() {
+			Em.Logger.info("SystemController save");
+			this.set('isEditing', false);
+			this.set('isCreating', false);
+			this.get("model").save();
+			Em.Logger.info(this.get("model").get('isCreating'));
+		}
+	}
 });
 
 
 
 App.ItemController = Ember.Controller.extend({
-  actions: {
-    doSomething: function(){
-      alert(this.model);
-      return true;
-    }
-  }
+	actions: {
+		doSomething: function() {
+			alert(this.model);
+			return true;
+		}
+	}
 });
 
 App.ApplicationAdapter = DS.RESTAdapter.extend({
-  namespace: 'api/v1'
+	namespace: 'api/v1'
 });
 
 // Model
 App.System = DS.Model.extend({
-  number : DS.attr('string'),
-  name: DS.attr('string'),
-  desc: DS.attr('string')
+	number: DS.attr('string'),
+	name: DS.attr('string'),
+	desc: DS.attr('string')
 });
 
 App.EditSystemView = Ember.TextField.extend({
-  didInsertElement: function() {
-    this.$().focus();
-  }
+	didInsertElement: function() {
+		this.$().focus();
+	}
 });
 
 Ember.Handlebars.helper('edit-system', App.EditSystemView);
