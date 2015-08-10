57bb45077a408f38d0e89391e9c31eb43c4fb93b
chengjf <sharpkingdom@gmail.com>
2015-08-10 10:40:32 +0800

    将restless对请求和响应的处理提取出来，作为公用。

---
+++ db_api/model/processor.py
@@ -0,0 +1,216 @@
+# -*- coding: utf-8 -*-
+from db_api import logger
+
+def get_single_preprocessor(instance_id=None, **kw):
+    """Accepts a single	argument, `instance_id`, the primary key of the
+    instance of	the model to get.
+
+    """
+    pass
+
+def get_single_postprocessor(result=None, **kw):
+    """Accepts a single	argument, `result`, which is the dictionary
+    representation of the requested instance of	the model.
+
+    """
+    pass
+    obj = result.copy()
+    result.clear()
+    result['system'] = obj
+
+
+def get_many_preprocessor(search_params=None, **kw):
+    """Accepts a single	argument, `search_params`, which is a dictionary
+    containing the search parameters for the request.
+
+    """
+    pass
+    logger.info("start get_many_preprocessor")
+    logger.info(search_params)
+    logger.info("end get_many_preprocessor")
+
+def get_many_postprocessor(result=None,	search_params=None, **kw):
+    """Accepts two arguments, `result`,	which is the dictionary
+    representation of the JSON response	which will be returned to the
+    client, and	`search_params`, which is a dictionary containing the
+    search parameters for the request (that produced the specified
+    `result`).
+
+    """
+    pass
+    logger.info("start get_many_postprocessor")
+    objects = result['objects']
+    result.clear()
+    result['systems'] = objects
+    logger.info(result)
+    logger.info("end get_many_postprocessor")
+
+def put_single_preprocessor(instance_id=None,	data=None, **kw):
+    """Accepts two arguments, `instance_id`, the primary key of	the
+    instance of	the model to patch, and	`data`,	the dictionary of fields
+    to change on the instance.
+
+    """
+    logger.info("start put_single_preprocessor")
+    logger.info(data)
+    system = data['system']
+    data.clear()
+    data.update(system)
+    logger.info("end put_single_preprocessor")
+    pass
+
+def put_single_postprocessor(result=None, **kw):
+    """Accepts a single	argument, `result`, which is the dictionary
+    representation of the requested instance of	the model.
+
+    """
+
+    logger.info("start put_single_postprocessor")
+    logger.info(result)
+    logger.info("end put_single_postprocessor")
+    pass
+
+def put_many_preprocessor(search_params=None,	data=None, **kw):
+    """Accepts two arguments: `search_params`, which is	a dictionary
+    containing the search parameters for the request, and `data`, which
+    is a dictionary representing the fields to change on the matching
+    instances and the values to	which they will	be set.
+
+    """
+    pass
+
+def put_many_postprocessor(query=None, data=None, search_params=None,
+			     **kw):
+    """Accepts three arguments:	`query`, which is the SQLAlchemy query
+    which was inferred from the	search parameters in the query string,
+    `data`, which is the dictionary representation of the JSON response
+    which will be returned to the client, and `search_params`, which is	a
+    dictionary containing the search parameters	for the	request.
+
+    """
+    pass
+
+
+def post_preprocessor(data=None, **kw):
+    """Accepts a single	argument, `data`, which	is the dictionary of
+    fields to set on the new instance of the model.
+
+    """
+    logger.info(data)
+    logger.info("start post_preprocessor")
+    system = data['system']
+    data.clear()
+    data.update(system)
+    logger.info(data)
+    logger.info("end post_preprocessor")
+    pass
+
+def post_postprocessor(result=None, **kw):
+    """Accepts a single	argument, `result`, which is the dictionary
+    representation of the created instance of the model.
+
+    """
+    logger.info("start post_postprocessor")
+    logger.info(result)
+    logger.info("end post_postprocessor")
+    pass
+
+def delete_single_preprocessor(instance_id=None, **kw):
+    """Accepts a single	argument, `instance_id`, which is the primary key
+    of the instance which will be deleted.
+
+    """
+    logger.info("start delete_single_preprocessor")
+    pass
+    logger.info(instance_id)
+    logger.info("end delete_single_preprocessor")
+
+def delete_single_postprocessor(was_deleted=None, **kw):
+    """Accepts a single	argument, `was_deleted`, which represents whether
+    the	instance has been deleted.
+
+    """
+    logger.info("start delete_single_postprocessor")
+    pass
+    logger.info(was_deleted)
+    logger.info("end delete_single_postprocessor")
+
+def delete_many_preprocessor(search_params=None, **kw):
+    """Accepts a single	argument, `search_params`, which is a dictionary
+    containing the search parameters for the request.
+
+    """
+    pass
+
+def delete_many_postprocessor(result=None, search_params=None, **kw):
+    """Accepts two arguments: `result`,	which is the dictionary
+    representation of which is the dictionary representation of	the JSON
+    response which will	be returned to the client, and `search_params`,
+    which is a dictionary containing the search	parameters for the
+    request.
+
+    """
+    pass 
+
+
+
+def foo(name='1'):
+    return bar()
+
+def hello(system_name):
+    def name(fn):
+        def wrapper(*args, **kwds):
+            print("hello, %s" % fn.__name__)
+            fn(system_name, *args, **kwds)
+            print("goodby, %s" % fn.__name__)
+        return wrapper
+    return name
+
+@hello('system')
+def bar(name):
+    logger.info('bar ' + name)
+
+def test(system_name):
+    def default_get_many_postprocessor(result=None, search_params=None, **kw):
+        """Accepts two arguments, `result`, which is the dictionary
+        representation of the JSON response which will be returned to the
+        client, and `search_params`, which is a dictionary containing the
+        search parameters for the request (that produced the specified
+        `result`).
+
+        """
+        pass
+        logger.info("start default_get_many_postprocessor " + system_name)
+        if result != None:
+            objects = result['objects']
+            result.clear()
+            b = {system_name : objects}
+            #result[name] = objects
+            result.update(b)
+            logger.info(result)
+            logger.info("end default_get_many_postprocessor " + system_name)
+        else:
+            logger.info('result is None')
+    return default_get_many_postprocessor
+
+
+def default_get_many_postprocessor(system_name=None, result=None, search_params=None, **kw):
+    """Accepts two arguments, `result`, which is the dictionary
+    representation of the JSON response which will be returned to the
+    client, and `search_params`, which is a dictionary containing the
+    search parameters for the request (that produced the specified
+    `result`).
+
+    """
+    pass
+    logger.info("start default_get_many_postprocessor " + system_name)
+    if result != None:
+        objects = result['objects']
+        result.clear()
+        b = {system_name : objects}
+        #result[name] = objects
+        result.update(b)
+        logger.info(result)
+        logger.info("end default_get_many_postprocessor " + system_name)
+    else:
+        logger.info('result is None')
--- db_api/model/system.py
+++ db_api/model/system.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import json
+from db_api import  app
+from db_api import logger
+
 from db_api.database import	db
 from db_api.database import	restless
-from db_api import	app
-import json
+from db_api.model import processor
+
 
 class System(db.Model):
 	"""
@@ -13,158 +17,6 @@ class System(db.Model):
 	number = db.Column(db.String(255), unique=True, nullable=False)
 	name = db.Column(db.String(255), nullable=False)
 	desc = db.Column(db.String(512))
-
-def get_single_preprocessor(instance_id=None, **kw):
-    """Accepts a single	argument, `instance_id`, the primary key of the
-    instance of	the model to get.
-
-    """
-    pass
-
-def get_single_postprocessor(result=None, **kw):
-    """Accepts a single	argument, `result`, which is the dictionary
-    representation of the requested instance of	the model.
-
-    """
-    pass
-    obj = result.copy()
-    result.clear()
-    result['system'] = obj
-
-
-def get_many_preprocessor(search_params=None, **kw):
-    """Accepts a single	argument, `search_params`, which is a dictionary
-    containing the search parameters for the request.
-
-    """
-    pass
-    print("start get_many_preprocessor")
-    print(search_params)
-    print("end get_many_preprocessor")
-
-def get_many_postprocessor(result=None,	search_params=None, **kw):
-    """Accepts two arguments, `result`,	which is the dictionary
-    representation of the JSON response	which will be returned to the
-    client, and	`search_params`, which is a dictionary containing the
-    search parameters for the request (that produced the specified
-    `result`).
-
-    """
-    pass
-    print("start get_many_postprocessor")
-    objects = result['objects']
-    result.clear()
-    result['systems'] = objects
-    print(result)
-    print("end get_many_postprocessor")
-
-def put_single_preprocessor(instance_id=None,	data=None, **kw):
-    """Accepts two arguments, `instance_id`, the primary key of	the
-    instance of	the model to patch, and	`data`,	the dictionary of fields
-    to change on the instance.
-
-    """
-    print("start put_single_preprocessor")
-    print(data)
-    system = data['system']
-    data.clear()
-    data.update(system)
-    print("end put_single_preprocessor")
-    pass
-
-def put_single_postprocessor(result=None, **kw):
-    """Accepts a single	argument, `result`, which is the dictionary
-    representation of the requested instance of	the model.
-
-    """
-
-    print("start put_single_postprocessor")
-    print(result)
-    print("end put_single_postprocessor")
-    pass
-
-def put_many_preprocessor(search_params=None,	data=None, **kw):
-    """Accepts two arguments: `search_params`, which is	a dictionary
-    containing the search parameters for the request, and `data`, which
-    is a dictionary representing the fields to change on the matching
-    instances and the values to	which they will	be set.
-
-    """
-    pass
-
-def put_many_postprocessor(query=None, data=None, search_params=None,
-			     **kw):
-    """Accepts three arguments:	`query`, which is the SQLAlchemy query
-    which was inferred from the	search parameters in the query string,
-    `data`, which is the dictionary representation of the JSON response
-    which will be returned to the client, and `search_params`, which is	a
-    dictionary containing the search parameters	for the	request.
-
-    """
-    pass
-
-
-def post_preprocessor(data=None, **kw):
-    """Accepts a single	argument, `data`, which	is the dictionary of
-    fields to set on the new instance of the model.
-
-    """
-    print(data)
-    print("start post_preprocessor")
-
-    system = data['system']
-    data.clear()
-    data.update(system)
-    print(data)
-    print("end post_preprocessor")
-    pass
-
-def post_postprocessor(result=None, **kw):
-    """Accepts a single	argument, `result`, which is the dictionary
-    representation of the created instance of the model.
-
-    """
-    print("start post_postprocessor")
-    print(result)
-    print("end post_postprocessor")
-    pass
-
-def delete_single_preprocessor(instance_id=None, **kw):
-    """Accepts a single	argument, `instance_id`, which is the primary key
-    of the instance which will be deleted.
-
-    """
-    print("start delete_single_preprocessor")
-    pass
-    print(instance_id)
-    print("end delete_single_preprocessor")
-
-def delete_single_postprocessor(was_deleted=None, **kw):
-    """Accepts a single	argument, `was_deleted`, which represents whether
-    the	instance has been deleted.
-
-    """
-    print("start delete_single_postprocessor")
-    pass
-    print(was_deleted)
-    print("end delete_single_postprocessor")
-
-def delete_many_preprocessor(search_params=None, **kw):
-    """Accepts a single	argument, `search_params`, which is a dictionary
-    containing the search parameters for the request.
-
-    """
-    pass
-
-def delete_many_postprocessor(result=None, search_params=None, **kw):
-    """Accepts two arguments: `result`,	which is the dictionary
-    representation of which is the dictionary representation of	the JSON
-    response which will	be returned to the client, and `search_params`,
-    which is a dictionary containing the search	parameters for the
-    request.
-
-    """
-    pass
 
 
 #restless.create_api(System, collection_name='systems',	url_prefix='/api/v1',methods=['GET', 'POST', 'DELETE','PUT'], results_per_page=100)
@@ -176,21 +28,22 @@ restless.create_api(
     collection_name='systems',
     results_per_page=-1,
     preprocessors={
-		'GET_MANY': [get_many_preprocessor],
-		'POST':	[post_preprocessor],
-		'GET_SINGLE':[get_single_preprocessor],
-		'PUT_SINGLE':[put_single_preprocessor],
-		'PUT_MANY':[put_many_preprocessor],
-		'DELETE_SINGLE':[delete_single_preprocessor],
-		'DELETE_MANY':[delete_many_preprocessor]
+		'GET_MANY': [processor.get_many_preprocessor],
+		'POST':	[processor.post_preprocessor],
+		'GET_SINGLE':[processor.get_single_preprocessor],
+		'PUT_SINGLE':[processor.put_single_preprocessor],
+		'PUT_MANY':[processor.put_many_preprocessor],
+		'DELETE_SINGLE':[processor.delete_single_preprocessor],
+		'DELETE_MANY':[processor.delete_many_preprocessor]
     },
     postprocessors={
-		'GET_MANY': [get_many_postprocessor],
-		'POST':	[post_postprocessor],
-		'GET_SINGLE':[get_single_postprocessor],
-		'PUT_SINGLE':[put_single_postprocessor],
-		'PUT_MANY':[put_many_postprocessor],
-		'DELETE_SINGLE':[delete_single_postprocessor],
-		'DELETE_MANY':[delete_many_postprocessor]
+		'GET_MANY': [processor.test('system')],
+		'POST':	[processor.post_postprocessor],
+		'GET_SINGLE':[processor.get_single_postprocessor],
+		'PUT_SINGLE':[processor.put_single_postprocessor],
+		'PUT_MANY':[processor.put_many_postprocessor],
+		'DELETE_SINGLE':[processor.delete_single_postprocessor],
+		'DELETE_MANY':[processor.delete_many_postprocessor]
     }
 )
+
