c36ec3719810753ebcc86988243a9fb10183c72e
chengjf <sharpkingdom@gmail.com>
2015-09-04 01:05:12 +0800

    增加了首页URLHandler。

---
+++ SparkDemo/src/com/chengjf/sparkdemo/module/index/IndexUrlHandler.java
@@ -0,0 +1,27 @@
+package com.chengjf.sparkdemo.module.index;
+
+import com.chengjf.sparkdemo.route.MyDefaultUrlHandler;
+
+import spark.Request;
+import spark.Response;
+
+/**
+ * 首页展示
+ * 
+ * @author sharp
+ * @date 2015-9-3
+ *
+ */
+public class IndexUrlHandler extends MyDefaultUrlHandler {
+
+	public IndexUrlHandler(String url) {
+		super(url);
+	}
+
+	@Override
+	public Object get(Request req, Response res) {
+		res.redirect("/todo");
+		return res;
+	}
+
+}
